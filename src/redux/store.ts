d677202ad4b736b5eb3ba28ac493727257da9826
Maisha-Chowa <maisha.chowa@gmail.com>
2023-12-31 11:18:22 +0600

    private-route

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,19 +1,19 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { api } from "./api/apiSice";
-import bookReducer from "./features/Books/bookSlice"
+import bookReducer from "./features/Books/bookSlice";
+import userReducer from "./features/User/userSlice";
 
 const store = configureStore({
   reducer: {
-   
     book: bookReducer,
     [api.reducerPath]: api.reducer,
+    user: userReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(api.middleware),
 });
 
-export type RootState 
-= ReturnType<typeof store.getState>;
+export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
 
 export default store;
